0b7af3cc44f97702edd276d92e04c1ba9789c45c
VictoriaRK <91897647+VictoriaRK@users.noreply.github.com>
2024-06-19 11:37:57 +0100

    Update 1482.java

--- 1482.java
+++ 1482.java
@@ -1,1 +1,12 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        // make m bouquets
+        // k adjacent flowers from garden
+        if (bloomDay.length < m*k){
+            return -1;
+        }
 
+        int[] spots = new int[bloomDay.length];
+        
+    }
+}
